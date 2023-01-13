59f9a80c650e7e0b0e98cd56fe65da4aeea5196d
Yassir Curbelo Halti <yassirch2001ch@gmail.com>
2023-01-13 17:15:44 +0000

    added crud controls

--- src/main/java/models/CarDealer.java
+++ src/main/java/models/CarDealer.java
@@ -55,18 +55,7 @@ public class CarDealer {
 
     //Cars crud controls
     public void cnCar(Car car) {//create new car
-
+        this.carInStock.add(car);
     }
 
-    public void rmCar(int id) {//remove car
-
-    }
-
-    public void edCar(Car car, int id) {//edit car
-
-    }
-
-    public void gCar(int id) {//get car
-
-    }
 }
